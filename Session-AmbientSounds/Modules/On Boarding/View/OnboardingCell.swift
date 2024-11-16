c9e47bac0f30222cf127b1ac8918e8c6c4ddeba3
AttabiqKhan <attabiqkhan@gmail.com>
2024-11-16 20:35:15 +0500

    On boarding screens somewhat implemented!

--- Session-AmbientSounds/Modules/On Boarding/View/OnboardingCell.swift
+++ Session-AmbientSounds/Modules/On Boarding/View/OnboardingCell.swift
@@ -41,7 +41,6 @@ class OnboardingCell: UICollectionViewCell {
         super.init(frame: frame)
         setupUI()
     }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
